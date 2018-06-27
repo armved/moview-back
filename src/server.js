83a8d8e6bf71fdc8dc85364e4044e91625e168f0
avetik <avetik.harutyunyan@logicify.com>
2018-06-27 23:28:59 +0300

    Remove passport.js references (Will implement custom auth middleware)

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
-const passport = require('koa-passport');
 const authRoutes = require('./routes').authRouter;
 
 const app = new Koa();
 
 app.use(logger());
 app.use(bodyParser());
-app.use(passport.initialize());
 app.use(authRoutes.routes());
 
 
